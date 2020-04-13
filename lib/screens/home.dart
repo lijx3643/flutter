a7dadcb8dc7dce4f5415454e5745f6845cbbcd3d
Larry <lukaoma@utexas.edu>
2020-04-13 04:11:44 -0500

    testing a push

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Title'),
+        title: Text('Update'),
       ),
       body: (_test != null)
           ? Center(
